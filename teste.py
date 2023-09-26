5f0b60d9e6a632c45ea2fbb819681ae8061981d1
RafaelNepomuceno <rafaelnepomucenope@gmail.com>
2023-09-26 00:22:13 -0300

    xablau

--- teste.py
+++ teste.py
@@ -1,37 +1,14 @@
-def adicao(a, b):
-    return a + b
-
-def subtracao(a, b):
-    return a - b
-
-def multiplicacao(a, b):
-    return a * b
-
-def divisao(a, b):
-    if b != 0:
-        return a / b
-    else:
-        return "Erro: divisão por zero!"
-
-print("Selecione a operação:")
-print("1 - Adição")
-print("2 - Subtração")
-print("3 - Multiplicação")
-print("4 - Divisão")
-
-escolha = input("Digite sua escolha (1/2/3/4): ")
+def numerador_lines(arquivo):
+    try:
+        with open(arquivo, 'r') as file:
+            for idx, line in enumerate(file, start=0):
+                print(f"{idx}{line.strip()}")
+    except FileNotFoundError:
+        print(f"O arquivo '{arquivo}' não foi encontrado.")
+    except Exception as e:
+        print(f"Ocorreu um erro: {e}")
 
 
-num1 = float(input("Digite o primeiro número: "))
-num2 = float(input("Digite o segundo número: "))
+arquivo = "exemplo.txt"  
+numerador_lines(arquivo)
 
-if escolha == '1':
-    print(num1, "+", num2, "=", adicao(num1, num2))
-elif escolha == '2':
-    print(num1, "-", num2, "=", subtracao(num1, num2))
-elif escolha == '3':
-    print(num1, "*", num2, "=", multiplicacao(num1, num2))
-elif escolha == '4':
-    print(num1, "/", num2, "=", divisao(num1, num2))
-else:
-    print("Escolha inválida!")
